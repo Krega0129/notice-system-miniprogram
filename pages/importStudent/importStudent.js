76476a89633a07f82b9a56e8b2ab933a21d3a6b3
Krega0129 <1841628911@qq.com>
2022-11-01 16:48:50 +0800

    fixed: 导入页面动态token

--- pages/importStudent/importStudent.js
+++ pages/importStudent/importStudent.js
@@ -2,6 +2,15 @@ import {
   BASE_URL
 } from '../../service/config'
 Page({
+  data: {
+    token: ''
+  },
+  onLoad() {
+    const token = wx.getStorageSync('authorization')
+    this.setData({
+      token: `https://krega.goodboycoder.top/up/?token=${token}`
+    })
+  },
   onShareAppMessage(options) {
     return {
       title: '校园消息推送助手',
